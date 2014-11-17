57b18d62366537fe12981127de1b89d7ae6056ba
darcwizrd <bpt3989@rit.edu>
2014-11-17 15:45:32 -0500

    no message

---
+++ GermBuster/GermBuster/Assets/Scripts/Enemy.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy : MonoBehaviour {
+
+	public int health;
+	public float speed = 1;
+	SpriteRenderer sprites;
+	public Transform target;
+	
+	void movement() //follow
+	{
+		float step = speed * Time.deltaTime;
+		transform.position = Vector3.MoveTowards(transform.position, target.position, step);
+	}
+	
+	void attack()
+	{
+		
+	}
+	
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		movement ();
+	}
+}
---
+++ GermBuster/GermBuster/Assets/Scripts/Player.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour 
+{
+
+	int health = 3;
+	float speed = 2f;
+	SpriteRenderer sprites;
+	
+	bool sick = false;
+	enum Disease{Headache, RunnyNose, Fever, StuffyNose};
+	
+
+	void movement()
+	{
+		
+		if(Input.GetKey("a"))
+		{
+			transform.position += Vector3.left * speed * Time.deltaTime;
+		}
+		
+		if(Input.GetKey("d"))
+		{
+			transform.position += Vector3.right * speed * Time.deltaTime;
+		}
+		
+		if(Input.GetKey("w"))
+		{
+			transform.position += Vector3.up * speed * Time.deltaTime;
+		}
+		
+		if(Input.GetKey("s"))
+		{
+			transform.position += Vector3.down * speed * Time.deltaTime;
+		}
+		
+		
+	}
+	
+	void attack()
+	{
+		if(sick)
+		{
+			Disease myDisease;
+			myDisease = Disease.Fever;
+			
+			if(myDisease == Disease.Fever)
+			{
+				speed = 1f;
+			}
+		}
+	}
+	
+	
+	
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		movement ();
+		
+	}
+}
